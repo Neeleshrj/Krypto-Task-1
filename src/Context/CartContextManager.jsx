9d1d68c09f95e6a41f4f38544c8370f4953eb622
Neelesh Ranjan Jha <neeleshranjan.jha2019@vitstudent.ac.in>
2022-07-29 23:58:44 +0530

    add to favourites added

--- src/Context/CartContextManager.jsx
+++ src/Context/CartContextManager.jsx
@@ -1,20 +1,57 @@
 import { createContext, useContext, useState, useEffect } from "react";
+import axios from "axios";
+
+import { useAuthContext } from "./AuthContextManager";
+
 
 export const CartContext = createContext();
 
-export default function CartContextProvider({children}){
-    const [cart, setCart] = useState([]);
+export default function CartContextProvider({ children }) {
+  const [cart, setCart] = useState([]);
+  const [favourite, setFavourite] = useState([]);
 
-    return(
-        <CartContext.Provider value={{
-            cart,
-            setCart
-        }}>
-            {children}
-        </CartContext.Provider>
-    )
+  const {userId} = useAuthContext();
+  
+  const getFavourites = () => {
+    axios.get("http://127.0.0.1:5000/favourites?userId"+userId).then((res) => {
+        console.log(res.data[res.data.length-1])
+        // setFavourite()
+    }).catch(e => console.log(e));
+  }
+
+  const updateFavourite = () => {
+    axios
+      .post("http://127.0.0.1:5000/favourites", {
+        favouriteList: favourite,
+        userId: userId,
+      })
+      .then((res) => {
+        console.log("favourite update");
+      })
+      .catch((e) => console.log(e));
+  };
+
+  useEffect(() => {
+    updateFavourite();
+    getFavourites()
+  }, [favourite]);
+  useEffect(()=> {
+    getFavourites()
+  },[])
+  return (
+    <CartContext.Provider
+      value={{
+        cart,
+        setCart,
+        favourite,
+        setFavourite,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
 }
 
 export function useCartContext() {
-    return useContext(CartContext);
-  }
+  return useContext(CartContext);
+}
--- src/components/Product/index.jsx
+++ src/components/Product/index.jsx
@@ -1,19 +1,74 @@
-import { Link, NavLink } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
 import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 import { MdOutlineAddShoppingCart } from "react-icons/md";
+
+import axios from "axios";
 
 import "./product.css";
 
 import { useCartContext } from "../../Context/CartContextManager";
+import { useAuthContext } from "../../Context/AuthContextManager";
 
 export default function Product({ data }) {
-  const {cart, setCart} = useCartContext();
+  const { cart, setCart, favourite, setFavourite } = useCartContext();
+  const { userId } = useAuthContext();
+  const [ isFavourite, setIsFavourite] = useState(false)
+  const [changed, setChanged] = useState(false);
+
+  const change = () => {
+    setChanged((prev) => !prev);
+  };
 
   const addItem = () => {
-    let items = [...cart]
-    items.push(data)
-    setCart(items)
+    let items = [...cart];
+    items.push(data);
+    setCart(items);
+  };
+
+
+  const checkFavourite = (val) => {
+    let i =0
+    while(i<favourite.length && favourite.length!=0){
+      if(favourite[i] === val){
+        setIsFavourite(true)
+        break
+      }
+      ++i;
+    }
+   
   }
+
+  const addToFavourite = (id) => {
+    let items = [...favourite];
+    let items2 = [];
+    let flag = 0;
+    let i = 0;
+    while(i < favourite.length){
+      if(favourite[i] === id){
+        console.log("match")
+        items2 = items.slice(0,i)
+        items = items.slice(i+1,favourite.length)
+        items = items2.concat(items)
+        setIsFavourite(false)
+        flag = 1;
+        break
+      }else{
+        ++i;
+      }
+    }
+
+    if(flag == 0){
+      items.push(id)
+    }
+    setFavourite(items)
+    change()
+    
+  };
+
+  useEffect(() => {
+    checkFavourite(data.id)
+  }, [changed]);
 
   return (
     <div className="product-container">
@@ -31,8 +86,22 @@ export default function Product({ data }) {
               <p className="rating">{data.rating}</p>
             </div>
             <div className="action-buttons-container">
-              <AiOutlineHeart className="action-buttons" />
-              <MdOutlineAddShoppingCart className="action-buttons" onClick={() => addItem()}/>
+              {isFavourite ? (
+                <AiFillHeart
+                  className="action-buttons"
+                  onClick={() => addToFavourite(data.id)}
+                />
+              ) : (
+                <AiOutlineHeart
+                  className="action-buttons"
+                  onClick={() => addToFavourite(data.id)}
+                />
+              )}
+
+              <MdOutlineAddShoppingCart
+                className="action-buttons"
+                onClick={() => addItem()}
+              />
             </div>
           </div>
         </div>
